5cc7ce4b91236f35bc870032dda0ef8db480feed
Neil Cookson <info@neilcookson.com>
2018-08-29 18:57:56 +1000

    Unit test setup
    Initialise unit test project and create first test methods

---
+++ UnitTestProject1/ProductsControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ProductsControllerTests
+    {
+        [DataRow(1)]
+        [DataTestMethod]
+        public void GeProduct_ReturnsProduct_MatchingId(int id)
+        {
+        }
+
+        [DataRow(999)]
+        [DataTestMethod]
+        public void GetProduct_ReturnsNotFound_NoMatchingId()
+        {
+            
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bfb3747-5eb5-476a-9b6a-d075d4a05c35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
