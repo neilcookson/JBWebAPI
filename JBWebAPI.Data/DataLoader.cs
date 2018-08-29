4babc1b1b7bc4afbf3ac24a39f93e8f2e802f409
Neil Cookson <info@neilcookson.com>
2018-08-29 19:24:57 +1000

    Add async reader for test data

---
+++ JBWebAPI.Data/DataLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBWebAPI.Data
+{
+    public static class DataLoader
+    {
+        public static async Task<string> ReadJsonDataAsync(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
