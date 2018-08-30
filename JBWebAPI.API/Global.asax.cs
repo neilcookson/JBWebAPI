e6324799e038750dad8e66c9f072aa4b67aef0ed
Neil Cookson <info@neilcookson.com>
2018-08-30 22:53:44 +1000

    Add and integrate autofac for DI

--- JBWebAPI.API/Global.asax.cs
+++ JBWebAPI.API/Global.asax.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using JBWebAPI.Data;
+using JBWebAPI.Data.Interfaces;
+using JBWebAPI.Data.Repositories;
+using System.Reflection;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace JBWebAPI.API
 {
@@ -11,6 +12,14 @@ namespace JBWebAPI.API
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<ProductRepository>().As<IProductRepository>().SingleInstance();
+            builder.RegisterType<TestDataService>().As<IDataService>().SingleInstance();
+            builder.RegisterType<ApiConfigurationSettings>().As<IConfigurationSettings>().SingleInstance();
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
